50ce04df1e2db4c7a141de554946ddf92ae11012
Kenny <zheng323@purdue.edu>
2018-11-07 20:43:39 -0500

    SignUp page Auth complete

--- app/src/main/java/com/example/tongan/myapplication/Activities/SignUpActivity.java
+++ app/src/main/java/com/example/tongan/myapplication/Activities/SignUpActivity.java
@@ -14,9 +14,13 @@ import android.widget.Toast;
 
 import com.example.tongan.myapplication.Classes.User;
 import com.example.tongan.myapplication.R;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -47,8 +51,7 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
-        mAuth = FirebaseAuth.getInstance();
-        buttonDeclarations();
+        init();
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,12 +60,42 @@ public class SignUpActivity extends AppCompatActivity {
                 boolean isValidInputs = isValidInputs();
 
                 if (isValidInputs) {
-                    addUserInformationToDatabase(firstName, lastName, email, password);
+                    signUpNewUserAndAddToDatabase(firstName, lastName, email, password);
                 }
             }
         });
     }
 
+    public void signUpNewUserAndAddToDatabase(final String firstName, final String lastName, final String email, final String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_LONG).show();
+                            Log.d(TAG, "createUserWithEmail:success");
+                            addUserInformationToDatabase(firstName, lastName, email, password);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+
+                            if (task.getException().getMessage().equals("The email address is badly formatted.")) {
+                                emailLayout.setError("Invalid email address");
+                            } else {
+                                emailLayout.setErrorEnabled(false);
+                            }
+                            if (password.length() < 6) {
+                                passwordLayout.setError("Password must be at least 6 characters");
+                            } else {
+                                passwordLayout.setErrorEnabled(false);
+                            }
+
+                        }
+
+                    }
+                });
+    }
 
     public boolean isValidInputs() {
 
@@ -73,18 +106,26 @@ public class SignUpActivity extends AppCompatActivity {
 
         if (firstName.equals("")) {
             firstNameLayout.setError("First name cannot be empty");
+        } else {
+            firstNameLayout.setErrorEnabled(false);
         }
 
         if (lastName.equals("")) {
             lastNameLayout.setError("Last name cannot be empty");
+        } else {
+            lastNameLayout.setErrorEnabled(false);
         }
 
         if (email.equals("")) {
             emailLayout.setError("Email cannot be empty");
+        } else {
+            emailLayout.setErrorEnabled(false);
         }
 
         if (password.equals("")) {
             passwordLayout.setError("Password cannot be empty");
+        } else {
+            passwordLayout.setErrorEnabled(false);
         }
 
         if (firstName.equals("") || lastName.equals("") || email.equals("") || password.equals("")) {
@@ -94,7 +135,10 @@ public class SignUpActivity extends AppCompatActivity {
         }
     }
 
-    public void buttonDeclarations() {
+    public void init() {
+
+        mAuth = FirebaseAuth.getInstance();
+
         firstNameLayout = findViewById(R.id.signup_firstName_layout);
         lastNameLayout = findViewById(R.id.signup_lastName_layout);
         emailLayout = findViewById(R.id.signup_email_layout);
