5317f08df27ec58b5173a74727016b02ea525bdf
Kenny <zheng323@purdue.edu>
2018-12-11 01:01:55 -0500

    bottom navigation bar

--- app/src/main/java/com/example/tongan/myapplication/Activities/MainActivity.java
+++ app/src/main/java/com/example/tongan/myapplication/Activities/MainActivity.java
@@ -1,15 +1,58 @@
 package com.example.tongan.myapplication.Activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.tongan.myapplication.Fragments.HomeFragment;
+import com.example.tongan.myapplication.Fragments.ProfileFragment;
 import com.example.tongan.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottom_navigation_menu);
+        bottomNavigation.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.home_frame, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()) {
+                case R.id.bottom_navigation_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+
+                case R.id.bottom_navigation_add:
+                    Intent intent = new Intent(MainActivity.this, AddPostActivity.class);
+                    startActivity(intent);
+                    break;
+
+                case R.id.bottom_navigation_profile:
+                    selectedFragment = new ProfileFragment();
+                    break;
+            }
+
+            if (selectedFragment == null) {
+                return false;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.home_frame, selectedFragment).commit();
+            return true;
+        }
+    };
+
 }
