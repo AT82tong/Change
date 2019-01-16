f3d1faf7d9d69f9bce179325e1f879f8b3409ef6
Tong An <an82@purdue.edu>
2019-01-16 05:31:27 -0500

    sad AT trigger

--- app/src/main/java/com/example/tongan/myapplication/Activities/AddPostActivity.java
+++ app/src/main/java/com/example/tongan/myapplication/Activities/AddPostActivity.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import com.example.tongan.myapplication.R;
 
 public class AddPostActivity extends AppCompatActivity {
-
+    private String sad_AT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_post);
+        sad_AT = "sad AT is going to sleep but AT wants to see Kenny and doesn't" +
+                "want to sleep alone, cry cry. AT missed Kenny so so so so much t_t" +
+                "q_q Q-Q QwQ p_q ;_; T-T TAT ToT QoQ cry cry";
+
     }
 }
