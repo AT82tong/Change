82727ef29ffea7ffa5519e1a475d68009b2d9157
kzheng <kzheng@blackboard.com>
2019-02-03 20:51:13 -0500

    update sign in page

--- app/src/main/java/com/example/tongan/myapplication/Activities/LoginActivity.java
+++ app/src/main/java/com/example/tongan/myapplication/Activities/LoginActivity.java
@@ -127,8 +127,8 @@ public class LoginActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
+                            passwordLayout.setError("Password doesn't match");
+                            //Toast.makeText(LoginActivity.this, "Authentication failed.",Toast.LENGTH_SHORT).show();
                         }
 
                     }
