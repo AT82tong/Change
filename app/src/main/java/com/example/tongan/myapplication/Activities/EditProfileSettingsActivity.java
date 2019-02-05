fa685a6e5bd894fe70b50a380d91af2515cb41a2
Kenny <zheng323@purdue.edu>
2019-02-05 18:25:45 -0500

    sign out button

--- app/src/main/java/com/example/tongan/myapplication/Activities/EditProfileSettingsActivity.java
+++ app/src/main/java/com/example/tongan/myapplication/Activities/EditProfileSettingsActivity.java
@@ -2,17 +2,32 @@ package com.example.tongan.myapplication.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.tongan.myapplication.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EditProfileSettingsActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
+
+    Button signOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+        signOutBtn = findViewById(R.id.sign_out_button);
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.getInstance().signOut();
+            }
+        });
     }
 }
