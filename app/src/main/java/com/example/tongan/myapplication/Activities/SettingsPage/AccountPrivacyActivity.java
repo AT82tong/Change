fed2ab05768e4fb32d7a44175d646f20286e97a1
Kenny <zheng323@purdue.edu>
2019-03-29 19:04:15 -0400

    back buttons

--- app/src/main/java/com/example/tongan/myapplication/Activities/SettingsPage/AccountPrivacyActivity.java
+++ app/src/main/java/com/example/tongan/myapplication/Activities/SettingsPage/AccountPrivacyActivity.java
@@ -2,10 +2,14 @@ package com.example.tongan.myapplication.Activities.SettingsPage;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.tongan.myapplication.R;
 
 public class AccountPrivacyActivity extends AppCompatActivity {
+
+    ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,14 @@ public class AccountPrivacyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_account_privacy);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 
+        backBtn = findViewById(R.id.back_button);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/tongan/myapplication/Activities/SettingsPage/AccountSecurityActivity.java
+++ app/src/main/java/com/example/tongan/myapplication/Activities/SettingsPage/AccountSecurityActivity.java
@@ -2,10 +2,14 @@ package com.example.tongan.myapplication.Activities.SettingsPage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.tongan.myapplication.R;
 
 public class AccountSecurityActivity extends AppCompatActivity {
+
+    ImageView backBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,14 @@ public class AccountSecurityActivity extends AppCompatActivity {
         setContentView(R.layout.activity_account_security);
         overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
 
+        backBtn = findViewById(R.id.back_button);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
     }
 }
