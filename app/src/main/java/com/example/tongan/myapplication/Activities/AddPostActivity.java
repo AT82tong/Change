d7a349a53320038b0976d390b4784160ee53e1c4
Kenny <zheng323@purdue.edu>
2019-03-29 21:57:40 -0400

    enabled location, can use FusedLocationProviderClient now

--- app/src/main/java/com/example/tongan/myapplication/Activities/AddPostActivity.java
+++ app/src/main/java/com/example/tongan/myapplication/Activities/AddPostActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.ImageView;
 
 import com.example.tongan.myapplication.R;
+import com.google.android.gms.location.FusedLocationProviderClient;
 
 public class AddPostActivity extends AppCompatActivity {
 
